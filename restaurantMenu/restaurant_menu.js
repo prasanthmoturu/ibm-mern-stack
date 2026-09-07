2dc7ebff9e49785f0f37509c42ea95fb746d8243
Prasanth <prasanthmoturu@gmail.com>
2026-09-07 09:59:51 -0400

    message

---
+++ restaurantMenu/restaurant_menu.js
@@ -0,0 +1,21 @@
+const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
+const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
+const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+
+const breakfastMenuItemsHTML = breakfastMenu
+    .map((item, index) => `<p>Item ${index + 1}: ${item}</p>`)
+    .join('');
+
+document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
+
+const mainCourseMenuItemsHTML = mainCourseMenu
+    .map((item, index) => `<p>Item ${index + 1}: ${item}</p>`)
+    .join('');
+
+document.getElementById('mainCourseMenuItems').innerHTML = mainCourseMenuItemsHTML;
+
+const dessertMenuItemsHTML = dessertMenu
+    .map((item, index) => `<p>Item ${index + 1}: ${item}</p>`)
+    .join('');
+
+document.getElementById('dessertMenuItems').innerHTML = dessertMenuItemsHTML;
